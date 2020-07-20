5cd61dd698ff231f99304deff9d87efeb87e3288
Donghyeok Kim <dhyeok1996@gmail.com>
2020-07-21 00:05:12 +0900

    Update 1181.py

--- 1181.py
+++ 1181.py
@@ -10,23 +10,8 @@ from sys import stdin
 
 N = int(stdin.readline())
 
-word_length = [False]*51
+words = list(set([stdin.readline().rstrip() for _ in range(N)]))
+words.sort(key=lambda x: (len(x), x))
+for word in words:
+    print(word)
 
-# 길이에 따라 같은 인덱스에 리스트로 추가
-for n in range(N):
-    word = stdin.readline().rstrip()
-    if word_length[len(word)] and word not in word_length[len(word)]:
-        word_length[len(word)].append(word)
-    else:
-        word_length[len(word)] = [word]
-
-# 작은 인덱스부터 정렬하며 출력
-for word_group in word_length:
-    if word_group:
-        word_group.sort()
-        print('\n'.join(word_group))
-
-# word_list = [stdin.readline().rstrip() for i in range(N)]
-# word_list = list(set(word_list))
-# word_list.sort(key=len)
-# print('\n'.join())
