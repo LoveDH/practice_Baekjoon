20903a6a629c52fd445529c142dd469bea45420f
Donghyeok Kim <dhyeok1996@gmail.com>
2020-08-07 23:25:14 +0900

    Update 1149.py

--- DP/1149.py
+++ DP/1149.py
@@ -10,11 +10,21 @@
 from sys import stdin
 
 def cost(N):
-    for num in range(N):
-        mem[num] = bills[num].index(max(bills[num]))
-    return 
+    # DP를 저장할 리스트 생성 및 초기화
+    RGB = [total_costs[0]]
+    for num in range(1,N):
 
+        local_costs = []
+        #이전 항의 값 + 다음 항의 최솟값(이전 항의 인덱스는 제외)
+        local_costs.append(total_costs[num][0] + min(RGB[num-1][1],RGB[num-1][2]))
+        local_costs.append(total_costs[num][1] + min(RGB[num-1][0],RGB[num-1][2]))
+        local_costs.append(total_costs[num][2] + min(RGB[num-1][0],RGB[num-1][1]))
+
+        RGB.append(local_costs)
+
+    return min(RGB[-1])
+
+#입력값
 N = int(stdin.readline())
-bills = [list(map(int, stdin.readline().split())) for _ in range(N)]
-mem = [-1]*N
+total_costs = [list(map(int, stdin.readline().split())) for _ in range(N)]
 print(cost(N))
